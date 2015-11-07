0447fdddb41410852ef9808580edc1671e0ee02c
mikuhime <miku@miku.io>
2015-11-07 14:08:09 +0100

    treten!

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ var client = new irc.Client(conf['connection']['server'], conf['bot']['nick'], {
   port: conf['connection']['port'],
   channels: conf['connection']['channels']
 })
+
+// treten
+var treten = true;
+
+client.addListener('action', function (from, to, text, message) {
+  var tritt = /tritt (\S*)$/
+  getreten = text.match(tritt)
+  if (getreten && treten) {
+    client.action(from, getreten[0])
+    treten = false
+  }
+});
+
+setInterval(function(){
+  treten = true;
+}, 3*60*1000)
